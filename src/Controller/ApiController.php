c07da77dfa1cb0aabbb862619191842aa31c661b
David <davidlecras@hotmail.com>
2021-02-21 14:14:27 +0100

    desrialization introduction

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,16 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
+    //Utiliser une API mis a dispo:
+
     /**
-     * @Route("/api", name="api")
+     * @Route("/liste-regions", name="list_regions")
      */
-    public function index(): Response
+    public function list_regions(SerializerInterface $serializerInterface): Response
     {
+        $regions = file_get_contents('https://geo.api.gouv.fr/regions');
+        //déserialisation des donnees par étapes:
+        // $regions_array = $serializerInterface->decode($regions, 'json');
+        // Pas d'appel de la BDD car non migré:
+        // $regions_object = $serializerInterface->denormalize($regions_array, 'App\Entity\Regions[]');
+
+        // désérialization avec la methode deserialize:
+        $regions_decode = $serializerInterface->deserialize($regions, 'App\Entity\Regions[]', 'json');
         return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+            'regions' => $regions_decode
         ]);
     }
 }
